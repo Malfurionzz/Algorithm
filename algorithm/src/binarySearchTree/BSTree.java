b1048ac764a82b170786a217f3c1d74679e7da8e
Malfurionzz <43919081+Malfurionzz@users.noreply.github.com>
2021-02-21 22:40:30 +0800

    二叉搜索树的非递归遍历和相应需要的辅助和测试

--- algorithm/src/binarySearchTree/BSTree.java
+++ algorithm/src/binarySearchTree/BSTree.java
@@ -1,19 +1,24 @@
 package binarySearchTree;
 
-public class BSTree<T extends Comparable<T>> {
-    private  BSTnode<T> root;
 
-    public class BSTnode<T extends Comparable<T>>{
+import java.time.temporal.Temporal;
+import java.util.Stack;
+import java.util.zip.ZipEntry;
+
+public class BSTree<T extends Comparable<T>> {
+    private BSTnode<T> root;
+
+    public class BSTnode<T extends Comparable<T>> {
         T key;
         BSTnode<T> left;
         BSTnode<T> right;
         BSTnode<T> parent;
 
-        public BSTnode(T key,BSTnode<T> parent,BSTnode<T> left,BSTnode<T> right){
-            this.key=key;
-            this.parent=parent;
-            this.left=left;
-            this.right=right;
+        public BSTnode(T key, BSTnode<T> parent, BSTnode<T> left, BSTnode<T> right) {
+            this.key = key;
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
         }
     }
 
@@ -24,30 +29,166 @@ public class BSTree<T extends Comparable<T>> {
             preOrder_r(tree.right);
         }
     }
+    private void preOrder_nr(BSTnode<T> tree) {
+        Stack<BSTnode> stack=new Stack<>();
+        stack.push(tree);
+        while (!stack.isEmpty()){
+            BSTnode tmp=stack.pop();
+            System.out.println(tmp.key + " ");
+            if (tmp.right!=null)
+                stack.push(tmp.right);
+            if (tmp.left!=null)
+                stack.push(tmp.left);
+        }
+    }
     public void preOrder() {
         preOrder_r(root);
+        preOrder_nr(root);
     }
 
     private void inOrder_r(BSTnode<T> tree) {
         if (tree != null) {
-            preOrder_r(tree.left);
+            inOrder_r(tree.left);
             System.out.println(tree.key + " ");
-            preOrder_r(tree.right);
+            inOrder_r(tree.right);
         }
     }
-    public void inOlder() {
+
+/*
+
+1)Push the current node to S and set current = current->left until current is NULL
+2) If current is NULL and stack is not empty then
+     a) Pop the top item from stack.
+     b) Print the popped item, set current = popped_item->right
+     c) Go to step 1.
+3) If current is NULL and stack is empty then we are done.
+ */
+    private void inOrder_nr(BSTnode<T> tree) {
+        Stack<BSTnode> stack=new Stack<>();
+        BSTnode tmp=tree;
+        do {
+            while (tmp!=null){
+                stack.push(tmp);
+                tmp=tmp.left;
+            }
+            if (tmp==null && !stack.isEmpty()){
+                tmp=stack.pop();
+                System.out.println(tmp.key + " ");
+                tmp=tmp.right;
+            }
+        }while (tmp!=null || !stack.isEmpty());
+    }
+
+    public void inOrder() {
         inOrder_r(root);
+        inOrder_nr(root);
     }
 
     private void postOrder_r(BSTnode<T> tree) {
         if (tree != null) {
-            preOrder_r(tree.left);
-            preOrder_r(tree.right);
+            postOrder_r(tree.left);
+            postOrder_r(tree.right);
             System.out.println(tree.key + " ");
         }
     }
-    public void postOlder() {
-        inOrder_r(root);
+/*
+1.1 Create an empty stack
+2.1 Do following while root is not NULL
+    a) Push root's right child and then root to stack.
+    b) Set root as root's left child.
+2.2 Pop an item from stack and set it as root.
+    a) If the popped item has a right child and the right child
+       is at top of stack(stack is not empty should considered otherwise exception throwed!), then remove the right child from stack,
+       push the root back and set root as root's right child.
+    b) Else print root's data and set root as NULL.
+2.3 Repeat steps 2.1 and 2.2 while stack is not empty.
+ */
+    private void postOrder_nr(BSTnode<T> tree) {
+        Stack<BSTnode> stack=new Stack<>();
+        BSTnode tmp=tree;
+        do {
+            while (tmp!=null){
+                if (tmp.right!=null)
+                    stack.push(tmp.right);
+                stack.push(tmp);
+                tmp=tmp.left;
+            }
+            tmp=stack.pop();
+            if (tmp.right!=null &&!stack.isEmpty()&& stack.peek().equals(tmp.right)){
+                stack.pop();
+                stack.push(tmp);
+                tmp=tmp.right;
+            }else {
+                System.out.println(tmp.key + " ");
+                tmp=null;
+            }
+        }while (!stack.isEmpty());
+    }
+    public void postOrder() {
+        postOrder_r(root);
+        postOrder_nr(root);
+    }
+
+    private void insert(BSTree<T> tree, BSTnode<T> node) {
+        BSTnode<T> y = null;
+        BSTnode<T> x = tree.root;
+        while (x != null) {
+            y = x;
+            if (node.key.compareTo(x.key)< 0) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        node.parent = y;
+        if (y == null) {//special case when tree is empty
+            tree.root = node;
+        } else if (node.key.compareTo(y.key) < 0) {
+            y.left = node;
+        } else {
+            y.right = node;
+        }
+    }
+    public void insert(T key){
+        BSTnode<T> node =new BSTnode<>(key,null,null,null);
+        if (node!=null){
+            insert(this,node);
+        }
+    }
+
+    private void print(BSTnode<T> tree, T key, int direction) {
+
+        if(tree != null) {
+
+            if(direction==0)    // tree是根节点
+                System.out.printf("%2d is root\n", tree.key);
+            else                // tree是分支节点
+                System.out.printf("%2d is %2d's %6s child\n", tree.key, key, direction==1?"right" : "left");
+
+            print(tree.left, tree.key, -1);
+            print(tree.right,tree.key,  1);
+        }
+    }
+
+    public void print() {
+        if (root != null)
+            print(root, root.key, 0);
+    }
+
+    public static void main(String[] args) {
+        final int[] arr = new int[]{1, 5, 4, 3, 2, 6};
+        int i, ilen;
+        BSTree<Integer> tree = new BSTree<>();
+
+        System.out.print("== 依次添加: ");
+        ilen = arr.length;
+        for (i = 0; i < ilen; i++) {
+            System.out.print(arr[i] + " ");
+            tree.insert(arr[i]);
+        }
+        System.out.println("== 树的详细信息: ");
+        tree.print();
+        tree.postOrder();
     }
 }
 
